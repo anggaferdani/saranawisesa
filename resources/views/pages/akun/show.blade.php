63592ff7b17d6bd70844404633ab68f5fb105e0a
Angga <111362630+eanggaa@users.noreply.github.com>
2023-04-16 21:57:56 +0700

    crud akun

--- resources/views/pages/akun/show.blade.php
+++ resources/views/pages/akun/show.blade.php
@@ -1,10 +1,10 @@
 @extends('templates.pages')
 @section('title')
 @section('header')
-<h1>Edit Akun</h1>
+<h1>Detail Akun</h1>
 <div class="section-header-breadcrumb">
   <div class="breadcrumb-item"><a href="#">Dashboard</a></div>
-  <div class="breadcrumb-item active"><a href="#">Edit Akun</a></div>
+  <div class="breadcrumb-item active"><a href="#">Detail Akun</a></div>
 </div>
 @endsection
 @section('content')
