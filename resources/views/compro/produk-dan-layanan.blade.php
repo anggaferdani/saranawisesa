8930bb820f12d4f1e9b8653b5d235014a0e6711e
Angga Wahyu Ferdani <eanggawahyu@gmail.com>
2023-06-20 22:33:48 +0700

    perbaikan compro dan pengadaan eproc

---
+++ resources/views/compro/produk-dan-layanan.blade.php
@@ -0,0 +1,28 @@
+@extends('templates.compro.pages')
+@section('title', 'Index')
+@section('content')
+<section class="py-5" style="background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url({{ asset('compro/produk-dan-layanan/thumbnail/'.$produk_dan_layanan["thumbnail"]) }}); background-size: cover; background-repeat: no-repeat; background-position: center;">
+  <div class="container py-5 h-100">
+    <div class="row py-5 justify-content-center align-items-center h-100">
+      <div class="col-md-10 text-white">
+        <h1 class="fw-bold title text-uppercase text-center mb-4">{{ $produk_dan_layanan->judul }}</h1>
+        <div class="fs-5 lh-sm">{!! $produk_dan_layanan->deskripsi !!}</div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section>
+  <div class="container-fluid">
+    @foreach($subproduk_dan_layanans as $subproduk_dan_layanan)
+      <div class="row my-5">
+        <div class="col-md-4 rounded-end-5" style="background: url({{ asset('compro/subproduk-dan-layanan/thumbnail/'.$subproduk_dan_layanan["thumbnail"]) }}); background-size: cover; background-repeat: no-repeat; background-position: center; min-height: 320px; border-left: 20px solid #920000;"></div>
+        <div class="col-md-8 mt-2 ps-2 ps-md-5">
+          <h1 class="fw-bold" style="color: #920000;">{{ $subproduk_dan_layanan->judul }}</h1>
+          <div class="mt-2 mt-md-4 lh-sm fs-5">{!! $subproduk_dan_layanan->deskripsi !!}</div>
+        </div>
+      </div>
+    @endforeach
+  </div>
+</section>
+@endsection
