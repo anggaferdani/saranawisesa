d1da9be26a0d97c80c77680fb48d722c681071c1
Angga Wahyu Ferdani <eanggawahyu@gmail.com>
2023-06-26 07:30:33 +0700

    perbaikan variable portofolio

--- resources/views/compro/portofolio.blade.php
+++ resources/views/compro/portofolio.blade.php
@@ -1,8 +1,8 @@
 @extends('templates.compro.pages')
 @section('title', 'Portofolio')
 @section('content')
-@foreach ($portofolio->portofolio_images->take(1) as $portofolios)
-<section style="height: 85vh; background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.2) 30%, rgba(0, 0, 0, 0.9) 100%), url({{ asset('compro/portofolio/image/'.$portofolios["image"]) }}); background-size: cover; background-repeat: no-repeat; background-position: center;">
+@foreach ($portofolio->portofolio_images->take(1) as $portofolio_image)
+<section style="height: 85vh; background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.2) 30%, rgba(0, 0, 0, 0.9) 100%), url({{ asset('compro/portofolio/image/'.$portofolio_image["image"]) }}); background-size: cover; background-repeat: no-repeat; background-position: center;">
 @endforeach
   <div class="container h-100">
     <div class="row py-4 h-100 align-items-end">
@@ -19,13 +19,13 @@
     <div class="row">
       <div class="col-md-6">
         <div class="row slider-for">
-          @foreach ($portofolio->portofolio_images as $portofolios)
-            <div class="col" style="height: 350px; background: url({{ asset('compro/portofolio/image/'.$portofolios["image"]) }}); background-size: cover; background-repeat: no-repeat;"></div>
+          @foreach ($portofolio->portofolio_images as $portofolio_image)
+            <div class="col" style="height: 350px; background: url({{ asset('compro/portofolio/image/'.$portofolio_image["image"]) }}); background-size: cover; background-repeat: no-repeat;"></div>
           @endforeach
         </div>
         <div class="row mt-2 slider-nav">
-          @foreach ($portofolio->portofolio_images as $portofolios)
-            <div class="col pe-3"><div style="height: 150px; background: url({{ asset('compro/portofolio/image/'.$portofolios["image"]) }}); background-size: cover; background-repeat: no-repeat;"></div></div>
+          @foreach ($portofolio->portofolio_images as $portofolio_image)
+            <div class="col pe-3"><div style="height: 150px; background: url({{ asset('compro/portofolio/image/'.$portofolio_image["image"]) }}); background-size: cover; background-repeat: no-repeat;"></div></div>
           @endforeach
         </div>
       </div>
